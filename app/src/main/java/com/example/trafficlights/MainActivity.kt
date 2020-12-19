e6b5e684125446db611782d07d6e04376118016d
ArtemiyYarovenko <tekkenik@mail.ru>
2020-12-20 03:01:13 +0700

    С помощью FusedLocationProviderClient сделал запросос на самую точную локацию
    Signed-off-by: ArtemiyYarovenko <tekkenik@mail.ru>

--- app/src/main/java/com/example/trafficlights/MainActivity.kt
+++ app/src/main/java/com/example/trafficlights/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.trafficlights
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailabilityLight
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvExample: TextView
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +33,33 @@ class MainActivity : AppCompatActivity() {
             //google play services не доступны на устройстве
             tvExample.text = "Google Play Services не доступны"
         }
+            getLocation()
 
 
     }
 
-     private fun isGooglePlayServicesAvailable () :Boolean {
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // если нет разрешения на использование геолокации
+            ActivityCompat.requestPermissions(this,
+                    arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION), 44)
+            return
+        }
+
+        // если геолокация есть, то делаем запрос ТЕКУЩЕЙ геолокации
+        fusedLocationProviderClient
+                .getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, null)
+                .addOnCompleteListener{
+            task ->
+            val location = task.result
+            if (location != null) {
+                Log.d("Locator", location.toString())
+            }
+        }
+
+    }
+
+    private fun isGooglePlayServicesAvailable () :Boolean {
         val googleApiAvailabilityLight = GoogleApiAvailabilityLight.getInstance()
         val status:Int = googleApiAvailabilityLight.isGooglePlayServicesAvailable(this)
         return status == ConnectionResult.SUCCESS

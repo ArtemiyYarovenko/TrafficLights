e42ead4851aaf07242ca3e7e72cca8c0ab973fef
ArtemiyYarovenko <tekkenik@mail.ru>
2020-12-19 21:46:43 +0700

    Настройка Google сервисов для дальнейшего использования для определения геолокации
    Signed-off-by: ArtemiyYarovenko <tekkenik@mail.ru>

--- app/src/main/java/com/example/trafficlights/MainActivity.kt
+++ app/src/main/java/com/example/trafficlights/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.trafficlights
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailabilityLight
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvExample: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvExample = findViewById(R.id.test)
+
+       if (isGooglePlayServicesAvailable()) {
+            //google play services доступны на устройстве
+           tvExample.text = "Google Play Services доступны"
+        }
+        else {
+            //google play services не доступны на устройстве
+            tvExample.text = "Google Play Services не доступны"
+        }
+
+
+    }
+
+     private fun isGooglePlayServicesAvailable () :Boolean {
+        val googleApiAvailabilityLight = GoogleApiAvailabilityLight.getInstance()
+        val status:Int = googleApiAvailabilityLight.isGooglePlayServicesAvailable(this)
+        return status == ConnectionResult.SUCCESS
     }
 }

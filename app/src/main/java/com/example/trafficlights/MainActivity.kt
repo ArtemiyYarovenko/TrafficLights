a7f7c587eba8f8b91a4df6ec39e580bfaac5942c
ArtemiyYarovenko <tekkenik@mail.ru>
2021-01-05 19:30:18 +0700

    Первоночальный набросок дизайна.
    Signed-off-by: ArtemiyYarovenko <tekkenik@mail.ru>

--- app/src/main/java/com/example/trafficlights/MainActivity.kt
+++ app/src/main/java/com/example/trafficlights/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.trafficlights
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun clickOnImage (view: View) {
+        Toast.makeText(this, "Тут будут дальнейшие действия после выбора проблемы", Toast.LENGTH_LONG).show()
     }
 }
